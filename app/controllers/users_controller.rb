cd2d9147251b89d32cd7cddeeeb599d5f66f4be8
tommy <kazu9su@gmail.com>
2017-07-14 16:46:07 +0900

    up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -48,14 +48,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
