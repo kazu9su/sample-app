39eabe52eab347b747f67577076857f917ee396a
tommy <kazu9su@gmail.com>
2017-08-01 10:16:13 +0900

    up

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'bootstrap-sass', '3.3.7'
 
 gem 'puma'
 
+gem 'bootsnap'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
                                   foreign_key: "follower_id",
                                   dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :active_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
